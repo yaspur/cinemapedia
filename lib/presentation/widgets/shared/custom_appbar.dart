79b750164ee2dccdb5290ee957221ebf2b0588c4
Yasid p <yapc6465@gmail.com>
2023-10-10 14:17:22 -0500

    navegacion entre tabs

--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -45,7 +45,7 @@ class CustomAppbar extends ConsumerWidget {
 
                     if(movie == null) return;
 
-                    context.push('/movie/${movie.id}');
+                    context.push('home/0/movie/${movie.id}');
 
                   });
 
